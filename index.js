21ee004a6989fdfc11801a5b290c4d5a68f9fd37
おDanịel.xyz <164250407+AikerDev@users.noreply.github.com>
2024-09-08 16:28:01 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var { aptoide, danbooru, fbdl, GDriveDl, GoogleImage, igdl, igstalk, mediafire, openAi, rule34, tiktokdl, tiktokuser, tiktokvid, tiktokSearch, pinterest, ppcouple, spotifySearch, spotify, soundSearch, xnxxdl, xvideosdl, ytsearch, ytmp3, ytmp4 } = require("./starlights")
+var { aptoide, danbooru, fbdl, GDriveDl, GoogleImage, igdl, igstalk, mediafire, mediafireV2, openAi, ChatGpt, ChatGptV2, rule34, tiktokdl, tiktokdlV2, tiktokuser, tiktokvid, tiktokStalk, tiktokSearch, pinterest, pinterestdl, ppcouple, spotifySearch, spotify, spotifydl, soundSearch, xnxxdl, xvideosdl, ytsearch, ytmp3, ytmp4 } = require("./starlights")
 
 module.exports = {
     aptoide: aptoide,
@@ -9,16 +9,23 @@ module.exports = {
     igdl: igdl,
     igstalk: igstalk,
     mediafire: mediafire,
+    mediafireV2: mediafireV2,
     openAi: openAi,
+    ChatGpt: ChatGpt,
+    ChatGptV2: ChatGptV2,
     rule34: rule34,
     spotifySearch: spotifySearch,
     spotify: spotify,
+    spotifydl: spotifydl,
     soundSearch: soundSearch,
     tiktokdl: tiktokdl,
+    tiktokdlV2: tiktokdlV2,
     tiktokuser: tiktokuser,
+    tiktokStalk: tiktokStalk,
     tiktokvid: tiktokvid,
     tiktokSearch: tiktokSearch,
     pinterest: pinterest,
+    pinterestdl: pinterestdl,
     ppcouple: ppcouple,
     xnxxdl: xnxxdl,
     xvideosdl: xvideosdl,
